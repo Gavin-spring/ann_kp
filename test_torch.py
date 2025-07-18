b0de699ff779ed7d0242ac65ecd219d077c17389
Gavin <youchang96824@outlook.com>
2025-07-18 15:53:16 +0100

    enhance the portable abilibty and enable fast command lines

--- test_torch.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-print(f"PyTorch Version: {torch.__version__}")
-print(f"CUDA Available: {torch.cuda.is_available()}")
-
-if torch.cuda.is_available():
-    print(f"CUDA Version used by PyTorch: {torch.version.cuda}")
-    print(f"Current GPU: {torch.cuda.get_device_name(0)}")
